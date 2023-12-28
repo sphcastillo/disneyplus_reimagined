207154dc1402735c4c12445621c2322a20304a63
Sophia Castillo <sphcastillo@gmail.com>
2023-12-28 15:20:12 -0800

    Added SearchInput inside Header

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import {Button} from '@/components/ui/button';
 export default function Home() {
   return (
     <main className="">
-      <h1>Disney Plus</h1>
-      <Button>Click</Button>
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ThemeToggler } from './ThemeToggler';
+import SearchInput from './SearchInput';
 
 function Header() {
   return (
-    <header>
-        <Link href="/">
+    <header 
+      className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900"
+    >
+        <Link href="/" className='mr-10'>
             <Image
                 src="https://links.papareact.com/a943ae"
                 alt="Disney logo"
@@ -17,8 +20,7 @@ function Header() {
         </Link>
         <div className='flex space-x-2'>
             {/** GenreDropdown */}
-            {/** SearchInput */}
-            {/** Theretoggler */}
+            <SearchInput />
             <ThemeToggler/>
         </div>
     </header>

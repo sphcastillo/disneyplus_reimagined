40bca19d607c35a71cdb562f041c75dc0b53a468
Sophia Castillo <sphcastillo@gmail.com>
2023-12-28 16:06:34 -0800

    GenreDropdown: successfully added API call with caching per request

--- components/GenreDropdown.tsx
+++ components/GenreDropdown.tsx
@@ -1,5 +1,15 @@
 import { Genres } from '@/typings';
-import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
+  import { ChevronDown } from 'lucide-react';
+  import Link from 'next/link';
 
 async function GenreDropdown() {
     const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
@@ -21,7 +31,22 @@ async function GenreDropdown() {
     console.log("data: ", data);
     
     return (
-        <div>GenreDropdown</div>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="text-white flex justify-center items-center">
+                Genre <ChevronDown className='ml-1' />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {data.genres.map((genre) => (
+                    <DropdownMenuItem key={genre.id}>
+                        <Link href={`/genre/${genre.id}?genre=${genre.name}`}>
+                            {genre.name}
+                        </Link>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 

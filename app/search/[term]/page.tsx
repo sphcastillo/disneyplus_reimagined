0abdbc69e60dae6a0ac5395ebb2bbe723e7d8e04
Sophia Castillo <sphcastillo@gmail.com>
2024-01-02 14:11:08 -0800

    added movie carousels to Search page

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,5 +1,6 @@
+import MoviesCarousel from '@/components/MoviesCarousel';
+import { getPopularMovies, getSearchedMovies } from '@/lib/getMovies';
 import { notFound } from 'next/navigation';
-import React from 'react'
 
 type Props = {
     params: {
@@ -7,16 +8,27 @@ type Props = {
     }
 };
 
-function SearchPage({ params: { term } }: Props) {
+async function SearchPage({ params: { term } }: Props) {
     if (!term) notFound();
 
     const termToUse = decodeURI(term); 
 
-    // Future: API call to get the searched movies
+    // API call to get the searched movies
+    const movies = await getSearchedMovies(termToUse);
+
     // API call to get the popular movies
+    const popularMovies = await getPopularMovies();
 
     return (
-        <div>Welcome to the search page: {termToUse}</div>
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex flex-col space-y-4 mt-32 xl:mt-42'>
+                <h1 className='text-6xl font-bold px-10'>Results for {termToUse}</h1>
+
+                {/* AI Suggestion */}
+                <MoviesCarousel title="Movies" movies={movies} isVertical/>
+                <MoviesCarousel title="You may also like..." movies={popularMovies} />
+            </div>
+        </div>
     )
 }
 

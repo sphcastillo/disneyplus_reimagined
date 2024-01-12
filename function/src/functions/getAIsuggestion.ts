b45d96446da7bfd9496fa183e3e784dac4b29c0b
Sophia Castillo <sphcastillo@gmail.com>
2024-01-12 13:06:50 -0800

    AISuggestion / route.ts ~ api/suggestions: created an API endpoint inside app Nextjs

--- function/src/functions/getAIsuggestion.ts
+++ function/src/functions/getAIsuggestion.ts
@@ -1,51 +1,47 @@
-import { 
-    app, 
-    HttpRequest, 
-    HttpResponseInit, 
-    InvocationContext 
-} from "@azure/functions";
+// https://disney-plus-clone.azurewebsites.net/api/getaisuggestion
 
-import OpenAI from 'openai';
-
-const openai = new OpenAI({
-
-    apiKey: process.env.OPENAI_API_KEY
-});
-
-
-export async function getAIsuggestion(
-    request: HttpRequest, 
+import {
+    app,
+    HttpRequest,
+    HttpResponseInit,
+    InvocationContext,
+  } from "@azure/functions";
+  
+  import OpenAI from "openai";
+  
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  
+  export async function getAISuggestion(
+    request: HttpRequest,
     context: InvocationContext
-): Promise<HttpResponseInit> {
+  ): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
+    const term = request.query.get("term");
 
-    const term = request.query.get('term');
-    console.log("Search Term >>> ", term);
-
+  
     const completion = await openai.chat.completions.create({
-        messages: [
-            {
-                role: 'system',
-                content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`
-            },
-            {
-                role: 'user',
-                content: `I like: ${term}`,
-            }
-        ],
-        model: 'gpt-3.5-turbo',
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`
+        },
+        {
+          role: "user",
+          content: `I like: ${term}`,
+        },
+      ],
     });
-
-    console.log("Completion >>> ", completion.choices[0]);
-
-    return { body: completion.choices[0].message.content || 'No suggestions found' };
-};
-
-app.http('getAIsuggestion', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    handler: getAIsuggestion
-});
-
-//https://disney-plus-clone.azurewebsites.net/api/getaisuggestion
-//https://disney-plus-clone.azurewebsites.net/api/getaisuggestion?
+  
+    console.log(completion.choices[0]);
+  
+    return { body: completion.choices[0].message.content || "No Suggestion" };
+  }
+  
+  app.http("getAISuggestion", {
+    methods: ["GET"],
+    authLevel: "anonymous",
+    handler: getAISuggestion,
+  });

143724d292e9c90953ad300996360c4b27d5ef13
Sophia Castillo <sphcastillo@gmail.com>
2024-01-18 15:58:33 -0800

    adding OpenAIAzureSuggestion/ installed Azure OpenAI into Nextjs app

--- app/api/suggestions/route.ts
+++ app/api/suggestions/route.ts
@@ -1,4 +1,15 @@
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const term = searchParams.get("term");
+
+    const res = await fetch(`https://disney-plus-clone.azurewebsites.net/api/getaisuggestion?term=${term}`, {
+        method: "GET",
+        next: {
+            revalidate: 60 * 60 * 24, // 24 hours
+        }
+    });
+
+    const message = await res.text();
+
+    return Response.json({ message });
 }

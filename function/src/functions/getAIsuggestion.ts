0387beb58f7e700b3228e909fc51e4330f0f23f9
Sophia Castillo <sphcastillo@gmail.com>
2024-02-02 11:26:52 -0800

    getAIsuggestion: added try/ catch statements

--- function/src/functions/getAIsuggestion.ts
+++ function/src/functions/getAIsuggestion.ts
@@ -20,24 +20,28 @@ import OpenAI from "openai";
     context.log(`Http function processed request for url "${request.url}"`);
     const term = request.query.get("term");
 
-  
-    const completion = await useOpenAI.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`
-        },
-        {
-          role: "user",
-          content: `I like: ${term}`,
-        },
-      ],
-    });
-  
-    console.log(completion.choices[0]);
-  
-    return { body: completion.choices[0].message.content || "No Suggestion" };
+    try {
+      const completion = await useOpenAI.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`
+          },
+          {
+            role: "user",
+            content: `I like: ${term}`,
+          },
+        ],
+      });
+    
+      console.log(completion.choices[0]);
+    
+      return { body: completion.choices[0].message.content || "No Suggestion" };
+    } catch (error) {
+      console.log("ERROR >>>", error);
+      return { body: "ERROR: No Suggestion"}
+    }
   }
   
   app.http("getAISuggestion", {

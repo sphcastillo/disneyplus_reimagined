19071cfb17751ab853aeea70243aa709e7ea95d6
sphcastillo <sphcastillo@gmail.com>
2024-06-04 16:49:43 -0700

    Addition: Chakra UI / modal ~ TVCard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header';
+import { Providers } from './providers';
 
 
 export const metadata: Metadata = {
@@ -16,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='bg-[#14143C]'>
+        <Providers>
           <Header />
           {children}
+        </Providers>
       </body>
     </html>
   )
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import getImagePath from '@/lib/getImagePath';
 import { Movie } from '@/typings'
 import Image from 'next/image';
+import Link from 'next/link';
 import { Ramabhadra } from 'next/font/google';
 
 const ramabhadra = Ramabhadra({ weight: "400", subsets: ['latin'] });
@@ -8,20 +9,22 @@ const ramabhadra = Ramabhadra({ weight: "400", subsets: ['latin'] });
 function MovieCard({ movie } : { movie : Movie }) {
   // console.log("movie: ", movie)
   return (
-    <div className='relative flex-shrink-0 cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shadow-lg'>
-    
-        <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/10 to-[#1A1C29]/80 z-10"/>
-        <div className={ramabhadra.className}>
-          <p className='absolute z-20 bottom-5 left-2 text-white text-[12px] md:text-[15px]'>{movie.title}</p>
-        </div>
-        <Image 
-            className='w-fit max-w-[210px] h-32 sm:max-w-[230px] sm:h-34 md:max-w-[280px] md:h-42 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm'
-            src= {getImagePath(movie.backdrop_path || movie.poster_path)}
-            alt= {movie.title}
-            width= {1920}
-            height= {1080}
-            key= {movie.id}
-        />
+    <div>
+      <div className='relative flex-shrink-0 cursor-pointer transform hover:scale-105 transition duration-200 ease-out hover:drop-shadow-lg'>
+      
+          <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/10 to-[#1A1C29]/80 z-10"/>
+          <div className={ramabhadra.className}>
+            <p className='absolute z-20 bottom-5 left-2 text-white text-[12px] md:text-[15px]'>{movie.title}</p>
+          </div>
+          <Image 
+              className='w-fit max-w-[210px] h-32 sm:max-w-[230px] sm:h-34 md:max-w-[280px] md:h-42 object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl rounded-sm'
+              src= {getImagePath(movie.backdrop_path || movie.poster_path)}
+              alt= {movie.title}
+              width= {1920}
+              height= {1080}
+              key= {movie.id}
+          />
+      </div>
     </div>
   )
 }

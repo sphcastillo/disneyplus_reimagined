6759b41c14cb7e8366b1666ebab5bec64a2a7954
sphcastillo <sphcastillo@gmail.com>
2024-06-03 20:25:57 -0700

    GenrePage: modified CSS so OpenAIAzureSuggestion can be seen at the top of the page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -22,13 +22,12 @@ async function GenrePage({ params: { id }, searchParams: { genre }}: Props) {
     return (
         <div className='max-w-7xl mx-auto bg-[#14143C]'>
             <div className={ramabhadra.className}>
-
-                <OpenAIAzureSuggestion term={genre}/>
-
-                <div className="flex flex-col space-y-5 mt-10 xl:mt-42">
-                    <div className='pt-[65px] md:pt-[80px]'>
+                <div className="pt-[85px]">
+                    <OpenAIAzureSuggestion term={genre}/>
+                </div>
+                <div className="flex flex-col space-y-5 mt-[50px]">
                         <h1 className="text-4xl sm:text-5xl font-bold px-10 text-white">Results from {genre}</h1>
-                    </div>
+
                     <MoviesCarousel title={`Genre`} movies={movies} isVertical/>
                 </div>
             </div>

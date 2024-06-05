8637817be57b227555a1bbc142e134fca063bb93
sphcastillo <sphcastillo@gmail.com>
2024-06-04 17:25:44 -0700

    MovieCard & TVCard modals: responsive

--- components/TVCard.tsx
+++ components/TVCard.tsx
@@ -6,18 +6,14 @@ import { Ramabhadra } from "next/font/google";
 import { useRef } from "react";
 import { useDisclosure } from "@chakra-ui/react";
 
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-} from "@chakra-ui/react";
+import { Modal, ModalOverlay, ModalContent } from "@chakra-ui/react";
 import { Card, CardBody, CardFooter } from "@chakra-ui/react";
 import { Stack } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
-import { CloseButton } from '@chakra-ui/react'
+import { CloseButton } from "@chakra-ui/react";
 import { Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Flex } from "@chakra-ui/react";
 
 const ramabhadra = Ramabhadra({ weight: "400", subsets: ["latin"] });
 
@@ -48,49 +44,63 @@ function TVCard({ series }: { series: Series }) {
         />
       </div>
 
-      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+      <Modal
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size={{ base: "90%", md: "md" }}
+      >
         <ModalOverlay />
-        <ModalContent bgGradient='linear(to-b, #010048, #090088 )'>
-        <CloseButton  
-                alignSelf='flex-end'
-                position='relative'
-                left={-3}
-                top={2}
-                onClick={onClose} 
-                mb={3}
-                size='sm' 
-                color="brand.100"
-                _hover={{ background: "blue", color: "white"}}
-              />
-          <Card maxW="md" bgGradient='linear(to-r, #140647, #200461)'>
-            <CardBody>
-              <Image
-                src={getImagePath(series.backdrop_path || series.poster_path)}
-                alt={series.title}
-                width={1920}
-                height={1080}
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md" color='brand.100'>{series.original_name}</Heading>
-                <Text color='brand.100'>
-                  {series.overview}
-                </Text>
-              </Stack>
-            </CardBody>
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button color="brand.100" bgGradient='linear(to-b, #B5B7BB, #CCCCCC)' _hover={{ background: "blue", color: "white"}}>
-                  Watch Now
-                </Button>
-                <Button color="brand.100" bgGradient='linear(to-r, #FFCC00, #200461)' _hover={{ background: "blue", color: "white"}}>
-                  Trailer
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
+        <ModalContent bgGradient="linear(to-b, #010048, #090088 )">
+          <CloseButton
+            alignSelf="flex-end"
+            position="relative"
+            left={-3}
+            top={2}
+            onClick={onClose}
+            mb={3}
+            size="sm"
+            color="brand.100"
+            _hover={{ background: "blue", color: "white" }}
+          />
+          <Flex justifyContent="center" alignItems="center">
+            <Card maxW={{ base: "90%", md: "md" }} bgGradient="linear(to-b, #010048, #090088 )">
+              <CardBody>
+                <Image
+                  src={getImagePath(series.backdrop_path || series.poster_path)}
+                  alt={series.title}
+                  width={1920}
+                  height={1080}
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" color="brand.100">
+                    {series.original_name}
+                  </Heading>
+                  <Text color="brand.100">{series.overview}</Text>
+                </Stack>
+              </CardBody>
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button
+                    color="brand.100"
+                    bgGradient="linear(to-b, #B5B7BB, #CCCCCC)"
+                    _hover={{ background: "blue", color: "white" }}
+                  >
+                    Watch Now
+                  </Button>
+                  <Button
+                    color="brand.100"
+                    bgGradient="linear(to-r, #FFCC00, #200461)"
+                    _hover={{ background: "blue", color: "white" }}
+                  >
+                    Trailer
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </Flex>
         </ModalContent>
       </Modal>
-
     </>
   );
 }

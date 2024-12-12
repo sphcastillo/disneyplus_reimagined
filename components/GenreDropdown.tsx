7406bc077fbe5c604028c49bb5bf94f3e21b7171
sphcastillo <sphcastillo@gmail.com>
2024-12-11 16:48:17 -0800

    Creation of NavigationLinks ~ Image/priority inside CarouselBanner ~ beginning alterations to GenreDropdown ~ additions added to Header

--- components/GenreDropdown.tsx
+++ components/GenreDropdown.tsx
@@ -1,61 +1,65 @@
-import { Genres } from '@/typings';
+import { Genres } from "@/typings";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu";
-import { ChevronDown } from 'lucide-react';
-import Link from 'next/link';
-import { Ramabhadra } from 'next/font/google';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
+import { Ramabhadra } from "next/font/google";
 
-
-const ramabhadra = Ramabhadra({ weight: "400", subsets: ['latin'] });
+const ramabhadra = Ramabhadra({ weight: "400", subsets: ["latin"] });
 
 async function GenreDropdown() {
-    const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
 
-    const options: RequestInit = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${process.env.TMBD_API_KEY}`
-        },
-        // same request will be cached for 24 hours
-        // ISR : Incremental Static Regeneration
-        next: {
-            revalidate: 60 * 60 * 24 
-        }
-    };
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMBD_API_KEY}`,
+    },
+    // same request will be cached for 24 hours
+    // ISR : Incremental Static Regeneration
+    next: {
+      revalidate: 60 * 60 * 24,
+    },
+  };
 
-    const response = await fetch(url, options);
-    const data = (await response.json()) as Genres;
+  const response = await fetch(url, options);
+  const data = (await response.json()) as Genres;
 
-    // showcase the genres
-    // console.log("genres: ", data.genres);
-    
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className="text-white flex justify-center items-center">
-                <div className={ramabhadra.className}>Genre</div>
-                <ChevronDown className='ml-1' />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className='z-[500] bg-gradient-to-b from-[#14143C] to-[#142878] text-white'>
-                <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                {data.genres.map((genre) => (
-                    <DropdownMenuItem key={genre.id} className=''>
-                        <Link href={`/genre/${genre.id}?genre=${genre.name}`} className=''>
-                            {genre.name}
-                        </Link>
-                    </DropdownMenuItem>
-                ))}
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+  // showcase the genres
+  // console.log("genres: ", data.genres);
+
+  return (
+    <div className={`${ramabhadra.className} flex`}>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="text-white flex justify-center items-center">
+          <div className='uppercase ml-7 sm:ml-0'>Genres</div>
+          <ChevronDown className="ml-1 hidden sm:block" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="z-[500] bg-gradient-to-b from-[#14143C] to-[#142878] text-white">
+          <DropdownMenuLabel>Select a Genre</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {data.genres.map((genre) => (
+            <DropdownMenuItem key={genre.id} className="">
+              <Link
+                href={`/genre/${genre.id}?genre=${genre.name}`}
+                className=""
+              >
+                {genre.name}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
 
 export default GenreDropdown;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-
-import SearchInput from './SearchInput';
-import GenreDropdown from './GenreDropdown';
-
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
+import { MdOutlineMoreVert } from "react-icons/md";
 import disneyPlusLogo from "@/images/disneyPluslogo.png";
+import NavigationLinks from "./NavigationLinks";
 
 function Header() {
   return (
-    <header className='fixed flex items-center justify-between p-3 top-0 z-50 w-full bg-gradient-to-b from-[#14143C] to-[#141450]'>
-        <Link href="/" className='mr-10'>
-            <Image
-                src={disneyPlusLogo}
-                alt="Disney+ logo"
-                width={100}
-                className='cursor-pointer'
-            />
+    <header className="fixed flex items-center justify-between p-3 top-0 z-50 w-full bg-gradient-to-b from-[#14143C] to-[#141450]">
+      <div className="flex items-center space-x-1 md:space-x-7">
+        <Link href="/" className="mr-2 sm:mr-10">
+          <Image
+            src={disneyPlusLogo}
+            alt="Disney+ logo"
+            width={100}
+            className="cursor-pointer"
+          />
         </Link>
-        <div className='flex space-x-4 md:space-x-7'>
-            <GenreDropdown />
-            <SearchInput />
+        <div className="hidden sm:block">
+          <NavigationLinks />
         </div>
+        <div className="flex items-center text-white sm:hidden">
+          <MdOutlineMoreVert />
+        </div>
+      </div>
+      <div className="flex space-x-4 md:space-x-7">
+        <GenreDropdown />
+        <SearchInput />
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;

48e2fa11b544fc96eea27e28d0261f0cccc9c800
sphcastillo <sphcastillo@gmail.com>
2024-12-16 12:49:05 -0800

    DisneyCategories: have ensured that the width / shap of the div remains consistent during the hover transition

--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -38,7 +38,7 @@ function CarouselBanner({ movies }: Props) {
                 height={810}
                 priority
                 src={getImagePath(movie.backdrop_path, true)}
-                className="h-[245px] sm:h-[320px] rounded-sm object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl"
+                className="h-[245px] sm:h-[320px] rounded-2xl object-cover object-center shadow-md shadow-gray-900 drop-shadow-xl"
               />
 
               <div className="absolute mt-0 top-0 pt-40 sm:pt-48 xl:pt-52 left-0 bg-transparent z-20 h-full w-full bg-gradient-to-r from-gray-900/30 via-transparent to-transparent p-5 space-y-5 text-white">

220dc33b7b1cc579ea3132c3ad62ad951a937bc0
sphcastillo <sphcastillo@gmail.com>
2024-12-16 14:16:29 -0800

    Modifications made around app

--- components/DisneyCategories.tsx
+++ components/DisneyCategories.tsx
@@ -71,7 +71,7 @@ const Categories = [
 function DisneyCategories() {
   const [hoveredId, setHoveredId] = useState<number | null>(null);
   return (
-    <div className="pt-3 sm:pt-6 py-2 flex flex-wrap justify-center gap-3">
+    <div className="pt-3 sm:pt-6 py-2 flex flex-wrap justify-center gap-3 md:gap-6 lg:gap-7">
       {Categories.map((category, index) => (
         <div
           className={`bg-[#131355] w-[82px] sm:w-[120px] h-[52px] sm:h-[90px] flex items-center justify-center rounded-xl shadow-2xl `}

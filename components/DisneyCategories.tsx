a783664aca2057eef3c429610c436019bb33f161
sphcastillo <sphcastillo@gmail.com>
2025-09-19 12:34:25 -0400

    DisneyCategories: CSS modifications / HeroCarousel: Image ~ object-right / object-center ~ height adjusted ~ width adjusted

--- components/DisneyCategories.tsx
+++ components/DisneyCategories.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 function DisneyCategories() {
   const [hoveredId, setHoveredId] = useState<number | null>(null);
   return (
-    <div className="pt-3 sm:pt-6 py-2 flex flex-wrap justify-center gap-3 md:gap-6 lg:gap-7">
+    <div className="pt-3 sm:pt-6 py-2 flex flex-wrap justify-center px-2 gap-6 lg:gap-7">
       {Categories.map((category, index) => (
         <div
           className={`bg-gray-800 w-[82px] sm:w-[120px] h-[52px] sm:h-[90px] flex items-center justify-center rounded-xl shadow-2xl `}
--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -19,27 +19,30 @@ function HeroCarousel() {
             ml-0 [&>*]:pl-0
             px-2
             [&>*]:mx-2
-            [&>*]:basis-[87%]  /* main slide always ~90% of screen width */
+            [&>*]:basis-[92%]
+            sm:[&>*]:basis-[94%]
+            md:[&>*]:basis-[96%]
+            lg:[&>*]:basis-[87%]  
           "
         >
           {HeroCarouselData.map((movie) => (
             <CarouselItem key={movie.id}>
-              <div className="xs:h-[220px] lg:h-[470px] w-full overflow-hidden rounded-xl">
+              <div className="h-[200px] sm:h-[325px] md:h-[350px] lg:h-[470px] w-full overflow-hidden rounded-xl">
                 <Image
                   src={movie.image}
                   alt={movie.title}
                   width={1920}
                   height={810}
                   priority
-                  className="h-full w-full object-cover"
+                  className="h-full w-full object-cover object-right sm:object-right md:object-right lg:object-center"
                 />
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
 
-        <CarouselPrevious className="left-6 top-1/2 -translate-y-1/2 h-10 w-10" />
-        <CarouselNext className="right-6 top-1/2 -translate-y-1/2 h-10 w-10" />
+        <CarouselPrevious className="hidden left-6 top-1/2 -translate-y-1/2 h-10 w-10" />
+        <CarouselNext className="hidden right-6 top-1/2 -translate-y-1/2 h-10 w-10" />
       </Carousel>
     </div>
   );

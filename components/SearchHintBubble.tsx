a97a99e63a31e6ebaaf2c7a42447f324aa448b23
sphcastillo <sphcastillo@gmail.com>
2025-09-23 12:19:59 -0400

    SearchHintBubble: CSS modification

--- components/SearchHintBubble.tsx
+++ components/SearchHintBubble.tsx
@@ -10,7 +10,7 @@ function SearchHintBubble() {
   if (!show) return null;
   return (
     <div className='absolute top-16 mobile:top-[72px] xs:top-[76px] right-1 mx-2 z-50'>
-        <div className="w-32 xxs500:w-36 xs:w-48 md:w-48 bounce-hint-delay-1 bg-blue-100 border border-blue-300 text-blue-800 text-[11px] rounded-lg pl-3 pr-1 py-2 shadow-md">
+        <div className="w-40 xxs:w-48 xs:w-48 md:w-48 bounce-hint-delay-1 bg-blue-100 border border-blue-300 text-blue-800 text-[11px] rounded-lg pl-3 pr-1 py-2 shadow-md">
         <div className="flex items-start justify-between gap-2">
           <span className="flex-1 break-words text-pretty">
           ✨ Ready for some movie magic? Just start searching.
